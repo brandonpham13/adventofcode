a629d5791bc9d1a1e64f9e49b7ac154fab624b51
Brandon Pham <brandonpham13@gmail.com>
2023-12-08 13:48:14 -0600

    adding first couple advent problems to repo

---
+++ problem-1.py
@@ -0,0 +1,42 @@
+# get first and last digit from each string
+# iterate through string to receive first digit
+# iterate backwards to receive last digit
+
+# one idea is to convert each string into an array of characters that can be indexed
+# another idea is to find first digit, then reverse the string and then find first digit again
+
+
+def trebuchet(cipher):
+    digits = []
+    password = [char for char in cipher]
+    password_backwards = password[::-1]
+    for char in password:
+        if char.isdigit() == True:
+            digits.append(char)
+            break
+        else:
+            continue
+    for char in password_backwards:
+        if char.isdigit() == True:
+            digits.append(char)
+            break
+        else:
+            continue
+    number = int(digits[0] + digits[1])
+    return number
+
+
+with open("elves.txt", "r") as file:
+    number_list = []
+    for line in file:
+        number_list.append(trebuchet(line.strip()))
+
+print(sum(number_list))
+
+
+# cipher = "brandon"
+# password = [char for char in cipher]
+# password_backwards = password[::-1]
+
+# print(password)
+# print(password_backwards)
---
+++ problem-2.py
@@ -0,0 +1,15 @@
+# make a dictionary correlating spelled numbers with their integer value
+
+number_dict = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "zero": 0,
+}
+
